e23ff8c2a578594f5912824cefd5a9fc06a491ea
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-04-13 00:39:27 +0500

    views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import Questions
 
 urlpatterns = [
-    
+    path('questions/', Questions.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Category, Question
+from django.http import JsonResponse, HttpRequest
+from django.views import View
+# class based views API for getting all categories and questions
 
-# Create your views here.
+class Questions(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        questions = Question.objects.all()
+        data = {
+            'questions': list(questions.values())
+        }
+        return JsonResponse(data)
+
